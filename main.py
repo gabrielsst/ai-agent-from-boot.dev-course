f25264c35b68a660bbfd5cb2313c58b98124c131
gabrielsst <gabrieeelsantos@gmail.com>
2026-08-04 20:36:04 -0300

    Creating the custom ai agent

---
+++ main.py
@@ -0,0 +1,45 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+import argparse
+
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+if api_key == None:
+  raise RuntimeError("environment variable wasn't found")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
+messages = [
+    {
+        "role": "user",
+        "content": args.user_prompt,
+    }
+]
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+)
+
+usage = response.usage
+
+if usage == None:
+    raise RuntimeError("There is no usage")
+
+if args.verbose:
+    print(f"User prompt: {args.user_prompt}")
+    print(f"Prompt tokens: {usage.prompt_tokens}")
+    print(f"Response tokens: {usage.completion_tokens}")
+
+print(f"Response:\n{response.choices[0].message.content}")
+
